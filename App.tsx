cee996d82133964b4ccd35da424b93a3a781473a
MichaelG <Michaelgani815@gmail.com>
2024-05-08 01:04:59 +0300

    intialize app with TS and TailwindCSS

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import "./App.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-white items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
