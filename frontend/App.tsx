27c571595756adf3eb49e3b6e707b1b4b8045f64
mikeg16345 <michaelgani815@gmail.com>
2024-05-14 09:45:58 +0300

    began working on home screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,8 +5,10 @@ import Navbar from './src/components/Navbar/Navbar';
 
 export default function App() {
   return (
-    <View className='flex-1 justify-center items-center bg-green-500 '>
-      <Navbar/>
+    <View className='flex-1 justify-center items-center bg-black '>
+      <View className='absolute bottom-0'>
+        <Navbar/>
+      </View>
       <Text className='text-black text-xl '>It indeed works!</Text>
       <StatusBar style="auto" />
     </View>
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 const Navbar = () => {
   return (
-    <View className='flex justify-between items-center'>
-      <Text>Home</Text>
-      <Text>Workout Plan</Text>
-      <Text>Diet Plan</Text>
+    <View className='flex flex-row w-full justify-around p-2 items-center bg-gray-500 rounded-lg '>
+      <Button title='Home' onPress={()=>{
+        console.log('do something')
+      }}/>
+      <Text className='text-white font-bold'>Home</Text>
+      <Text className='text-white font-bold'>Workout Plan</Text>
+      <Text className='text-white font-bold'>Diet Plan</Text>
     </View>
   )
 }
