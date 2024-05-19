24f7ab709d7ce00360d4fd344d40d2de87cb436f
mikegani <michael.g.perfetto@gmail.com>
2024-05-19 15:46:02 +0300

    fixed issue with web bundling

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import "./global.css";
+import { verifyInstallation } from "nativewind";
 
 export default function App() {
+  verifyInstallation();
   return (
-    <View className="flex-1 justify-center items-center bg-red-500 ">
-      <Text className="text-black text-xl ">It doesnt works!</Text>
-      <Text className="text-black text-xl ">It doesnt works!</Text>
-
+    <View className="flex-1 justify-center items-center bg-green-500 ">
+      <Text className="text-black text-xl ">It actually works!</Text>
       <StatusBar style="auto" />
-      <Text>Fuck your login page. We accepting this shit.</Text>
     </View>
   );
 }
