47965b0b4a80547f550ca6d1939680248acfd104
NoamMoskowitz <noammz101@gmail.com>
2024-05-22 17:47:06 +0300

    fixed icons

--- frontend/src/components/DietPlan/CollapsableItem.tsx
+++ frontend/src/components/DietPlan/CollapsableItem.tsx
@@ -4,7 +4,8 @@ import { ListItem } from '@rneui/themed';
 import chicken from '../../../assets/images/chicken-icon.svg'
 import bread from '../../../assets/images/bread-icon.svg'
 import vegetable from '../../../assets/images/vegetable-icon.svg'
-import { Avatar, Icon, Image } from 'react-native-elements';
+import { Avatar, Image } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 export default function CollapsableItem({ meal, title }) {
 
@@ -24,13 +25,15 @@ export default function CollapsableItem({ meal, title }) {
             >
                 <View className='flex-row'>
                     <ListItem className='flex-row'>
-                        <Avatar source={chicken} size={'medium'} rounded />
+                        <Icon name='drumstick-bite' />
                         <Text>{`${meal.totalProtein}g`}</Text>
                     </ListItem>
                     <ListItem>
+                        <Icon name='bread-slice' />
                         <Text>{`${meal.totalCarbs}g`}</Text>
                     </ListItem>
                     <ListItem>
+                        <Icon name='carrot' />
                         <Text>{`${meal.totalVeggies}`}</Text>
                     </ListItem>
                 </View>
