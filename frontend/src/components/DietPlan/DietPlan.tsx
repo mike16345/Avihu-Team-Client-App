eda095a7ef3191274f2ef1c6aa56631b7e2b82dd
NoamMoskowitz <noammz101@gmail.com>
2024-05-31 11:48:52 +0300

    changed button location and fixed dietplan data

--- frontend/src/components/DietPlan/DietPlan.tsx
+++ frontend/src/components/DietPlan/DietPlan.tsx
@@ -20,7 +20,18 @@ export default function DietPlan() {
         <ScrollView className="w-screen h-screen flex-1 bg-black relative">
             <ImageBackground source={logoBlack} className="w-screen h-[30vh]" />
 
-        
+            <View className='flex-row justify-center gap-9 pb-6'>
+                <TouchableOpacity onPress={()=>setUiView(UI_TYPES.PROTEIN)}>
+                    <View className='bg-emerald-300 p-2 rounded'>
+                        <Text className='text-lg font-bold'>חלבונים</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>setUiView(UI_TYPES.CARBS)}>
+                    <View className='bg-emerald-300 p-2 rounded'>
+                        <Text className='text-lg font-bold'>פחמימות</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
 
             {meals.map((meal) => (
                 <View className='p-4 rtl items-center'>
@@ -31,7 +42,7 @@ export default function DietPlan() {
 
                         <Text className='text-white'>{meal.optOne}</Text> :
 
-                        <View className='flex-row w-[100vw] flex-wrap p-1 justify-center'>
+                        <View className='flex-row-reverse w-[100vw] flex-wrap p-1 justify-center'>
                             {meal.optOne.map((opt)=>(<Text className='text-white px-1'>{opt} /</Text>))}
                         </View>}
 
@@ -46,18 +57,6 @@ export default function DietPlan() {
                 </View>
             ))}
 
-                <View className='flex-row justify-center gap-9 pb-6'>
-            <TouchableOpacity onPress={()=>setUiView(UI_TYPES.PROTEIN)}>
-                <View className='bg-emerald-300 p-2 rounded'>
-                    <Text className='text-lg font-bold'>חלבונים</Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=>setUiView(UI_TYPES.CARBS)}>
-                <View className='bg-emerald-300 p-2 rounded'>
-                    <Text className='text-lg font-bold'>פחמימות</Text>
-                </View>
-            </TouchableOpacity>
-        </View>
 
         <View className='absolute top-[35vh]'>
             {uiView === UI_TYPES.CARBS && <CarbTable setUiState={setUiView} uiStates={UI_TYPES} />}
