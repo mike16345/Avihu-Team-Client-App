5442e2481369a92eac22441fba33674be07e533a
mikeg16345 <michaelgani815@gmail.com>
2024-06-02 21:58:06 +0300

    added weights and selected day styles to calendar

--- frontend/src/components/Calendar/WeightCalendar.tsx
+++ frontend/src/components/Calendar/WeightCalendar.tsx
@@ -5,55 +5,66 @@ import DayComponent from "./Day";
 import { useMemo, useState } from "react";
 import DateUtils from "@/utils/dateUtils";
 import { Colors } from "@/constants/Colors";
-import { MarkedDates } from "react-native-calendars/src/types";
+import { myWeighIns } from "@/constants/MyWeight";
+import { MarkingProps } from "react-native-calendars/src/calendar/day/marking";
+
+export interface ExtendedMarking extends MarkingProps {
+  weight?: number;
+  customStyles?: any;
+}
+interface MarkedDays {
+  [key: string]: ExtendedMarking;
+}
 
 const WeightCalendar = () => {
   const [selected, setSelected] = useState(DateUtils.getCurrentDate("YYYY-MM-DD"));
+  const marked: MarkedDays = useMemo(() => {
+    const marks: MarkedDays = {};
 
-  const marked: MarkedDates = useMemo(() => {
-    return {
-      [selected]: {
-        selected: true,
+    myWeighIns.forEach((weighIn) => {
+      const dateString = weighIn.date.toISOString().split("T")[0];
+
+      marks[dateString] = {
+        selected: selected === dateString,
         selectedColor: Colors.primary,
         dotColor: Colors.warning,
         marked: true,
-        periods: [{ startingDay: true, endingDay: false, color: Colors.danger }],
+        selectedTextColor: Colors.danger,
+        dots: [
+          { color: Colors.primaryDark, key: dateString, selectedDotColor: Colors.primaryLight },
+        ],
+        weight: weighIn.weight,
+      };
+    });
 
-        selectedTextColor: Colors.dark,
-        dots: [{ color: Colors.primaryDark, key: selected, selectedDotColor: Colors.primaryLight }],
-      },
-    };
+    return marks;
   }, [selected]);
 
-  console.log("marked", marked);
-  console.log("selected", selected);
-
   return (
-    <View className="flex-1">
-      <CalendarProvider date="">
+    <View className="flex-1 ">
+      <CalendarProvider date={selected}>
         <Calendar
           markedDates={marked}
           enableSwipeMonths
           onDayPress={(day) => {
             setSelected(day.dateString);
           }}
+          dayComponent={({ date, state, marking }) => {
+            return (
+              <DayComponent
+                date={date?.day || 0}
+                state={state || ""}
+                marking={marking}
+                key={1}
+                onPress={() => {
+                  if (!date) return;
+                  console.log("setting selected", date?.day);
+                  setSelected(date.dateString);
+                }}
+              />
+            );
+          }}
           hideExtraDays
-          // dayComponent={({ date, state, marking }) => {
-          //   console.log(`day (${date?.day})states :${state}`);
-
-          //   return (
-          //     <DayComponent
-          //       date={date?.day || 0}
-          //       state={state || ""}
-          //       marking={marking?.customStyles || { weight: "88kg" }}
-          //       key={1}
-          //       onPress={() => {
-          //         console.log("setting selected", date?.day);
-          //         setSelected(date.dateString);
-          //       }}
-          //     />
-          //   );
-          // }}
           style={{ borderRadius: 4 }}
           theme={darkTheme}
         />
