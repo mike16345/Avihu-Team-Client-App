2751ab78e98de320c1ba9a429542f9e9352e6cd3
mikegani <michael.g.perfetto@gmail.com>
2024-06-04 14:39:19 +0300

    added routers to app server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,21 +3,24 @@ import dotenv from "dotenv";
 import path from "path";
 import http from "http";
 import cors from "cors";
+import userRouter from "./routers/users";
+import weighInsRouter from "./routers/weighIns";
 import "./db/mainConnection";
 
 dotenv.config();
 
+const PORT = "3002";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/users", userRouter);
+app.use("/weighIns", weighInsRouter);
 
 const server = http.createServer(app);
 
-const port = "3002";
-
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
