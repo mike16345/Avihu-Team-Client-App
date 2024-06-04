4a069412b8c83dc4f39ab49f8d96393912de9887
mikegani <michael.g.perfetto@gmail.com>
2024-06-04 15:27:06 +0300

    updated user error handling

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,55 +2,119 @@ import { userServices } from "../services/userService";
 import { Request, Response } from "express";
 import { UserSchemaValidation } from "../models/userModel";
 
-class userController {
+class UserController {
   addUser = async (req: Request, res: Response) => {
-    const data = {
-      name: req.body.name,
-      email: req.body.email,
-    };
+    try {
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+      };
 
-    const { error, value } = UserSchemaValidation.validate(data);
+      const { error, value } = UserSchemaValidation.validate(data);
 
-    if (error) {
-      res.send(error.message);
-    } else {
+      if (error) {
+        return res.status(400).send(error.message);
+      }
+
       const user = await userServices.createUser(value);
       res.status(201).send(user);
+    } catch (err: any) {
+      res.status(500).send(err.message);
     }
   };
 
   getUsers = async (req: Request, res: Response) => {
-    const users = await userServices.getUsers();
-    res.send(users);
+    try {
+      const users = await userServices.getUsers();
+      res.status(200).send(users);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 
   getUser = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const user = await userServices.getUser(id);
-    res.send(user);
+    try {
+      const id = req.params.id;
+      const user = await userServices.getUser(id);
+
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+
+      res.status(200).send(user);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 
   getUserByEmail = async (req: Request, res: Response) => {
-    const email = req.params.email;
-    const user = await userServices.getUserByEmail(email);
-    res.send(user);
+    try {
+      const email = req.params.email;
+      const user = await userServices.getUserByEmail(email);
+
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+
+      res.status(200).send(user);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 
   updateUser = async (req: Request, res: Response) => {
-    const user = await userServices.updateUser(req.body);
-    res.send(user);
+    try {
+      const id = req.params.id;
+      const data = req.body;
+      data._id = id;
+
+      const { error, value } = UserSchemaValidation.validate(data);
+
+      if (error) {
+        return res.status(400).send(error.message);
+      }
+
+      const user = await userServices.updateUser(value);
+
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+
+      res.status(200).send(user);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 
   updateManyUsers = async (req: Request, res: Response) => {
-    const users = await userServices.updateManyUsers(req.body);
-    res.send(users);
+    try {
+      const { error, value } = UserSchemaValidation.validate(req.body);
+
+      if (error) {
+        return res.status(400).send(error.message);
+      }
+
+      const users = await userServices.updateManyUsers(value);
+      res.status(200).send(users);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 
   deleteUser = async (req: Request, res: Response) => {
-    const id = req.query.id;
-    const resp = await userServices.deleteUser(id as string);
-    res.send(resp);
+    try {
+      const id = req.params.id;
+      const user = await userServices.deleteUser(id);
+
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+
+      res.status(200).send(user);
+    } catch (err: any) {
+      res.status(500).send(err.message);
+    }
   };
 }
 
-export const UserController = new userController();
+export const userController = new UserController();
