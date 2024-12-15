d4e4359b74b46ad7a8f5bc7cd5c32357eede63c0
Noam Moskowitz <noammz101@gmail.com>
2024-12-15 15:33:07 +0200

    switched checkbox to native icon

--- frontend/src/components/Login/PasswordIndicatorItem.tsx
+++ frontend/src/components/Login/PasswordIndicatorItem.tsx
@@ -1,9 +1,8 @@
 import useStyles from "@/styles/useGlobalStyles";
 import React from "react";
-import { Checkbox } from "react-native-paper";
-
 import { Text } from "../ui/Text";
 import { View } from "react-native";
+import NativeIcon from "../Icon/NativeIcon";
 
 interface PasswordIndicatorItemProps {
   checked: boolean;
@@ -11,11 +10,15 @@ interface PasswordIndicatorItemProps {
 }
 
 const PasswordIndicatorItem: React.FC<PasswordIndicatorItemProps> = ({ checked, message }) => {
-  const { colors, layout, spacing } = useStyles();
+  const { colors, layout, spacing, fonts } = useStyles();
   return (
     <View style={[layout.flexRow, layout.justifyEnd, layout.itemsCenter, spacing.gapDefault]}>
       <Text style={colors.textOnBackground}>{message}</Text>
-      <Checkbox status={"checked"} color={colors.textSuccess.color} disabled={!checked} />
+      <NativeIcon
+        library="Feather"
+        name="check"
+        style={[checked ? colors.textSuccess : colors.textDanger, fonts.xl]}
+      />
     </View>
   );
 };
