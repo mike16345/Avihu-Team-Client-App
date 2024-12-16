5c7a659649eb89188eb5a24a7b4e159425623534
Noam Moskowitz <noammz101@gmail.com>
2024-12-16 13:53:14 +0200

    implemented request throttling on pulldown to refresh hook

--- frontend/src/hooks/usePullDownToRefresh.tsx
+++ frontend/src/hooks/usePullDownToRefresh.tsx
@@ -2,11 +2,27 @@ import React, { useState } from "react";
 
 const usePullDownToRefresh = () => {
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [refreshCount, setRefreshCount] = useState<number>(0);
 
-  const refresh = async (apiCall: () => Promise<any>) => {
+  const startRefreshCount = setTimeout(() => {
+    setRefreshCount(0);
+  }, 60000);
+
+  const handleRefresh = async (apiCall: () => Promise<any>) => {
+    setRefreshCount((prev) => prev + 1);
     setIsRefreshing(true);
     await apiCall();
     setIsRefreshing(false);
+  };
+
+  const refresh = (apiCall: () => Promise<any>) => {
+    if (refreshCount > 3) return;
+
+    if (refreshCount == 0) {
+      startRefreshCount;
+    }
+
+    handleRefresh(apiCall);
   };
 
   return { isRefreshing, refresh };
