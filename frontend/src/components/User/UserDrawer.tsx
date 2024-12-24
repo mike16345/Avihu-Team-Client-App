ac93e4975f6c4d12f04f801dda45f66a6fdd1a39
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-12-24 15:34:35 +0200

    fix for iphone stlying

--- frontend/src/components/User/UserDrawer.tsx
+++ frontend/src/components/User/UserDrawer.tsx
@@ -1,18 +1,18 @@
 import useStyles from "@/styles/useGlobalStyles";
-import { ScrollView, View } from "react-native";
+import { View } from "react-native";
 import RightDrawer from "../ui/RightDrawer";
 import { Text } from "../ui/Text";
-import UserInfoContainer from "./UserInfoContainer";
 import { Button } from "react-native-paper";
 import { useUserDrawer } from "@/store/userDrawerStore";
 import { useUserStore } from "@/store/userStore";
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import NativeIcon from "../Icon/NativeIcon";
+import UserDetailContainer from "./UserDetailContainer";
 
 const UserDrawer = () => {
   const { colors, common, fonts, layout, spacing, text } = useStyles();
   const { openUserDrawer, setOpenUserDrawer } = useUserDrawer();
-  const { setCurrentUser } = useUserStore();
+  const { currentUser, setCurrentUser } = useUserStore();
   const sessionStorage = useAsyncStorage("sessionToken");
 
   const logUserOut = () => {
@@ -27,12 +27,32 @@ const UserDrawer = () => {
       onClose={() => setOpenUserDrawer(false)}
       children={
         <View
-          style={[layout.sizeFull, spacing.pdHorizontalMd, spacing.pdStatusBar, spacing.gapDefault]}
+          style={[
+            layout.sizeFull,
+            layout.justifyEvenly,
+            spacing.pdHorizontalMd,
+            spacing.pdStatusBar,
+          ]}
         >
           <Text style={[text.textRight, text.textBold, colors.textOnBackground, fonts.lg]}>
             פרטי משתמש
           </Text>
-          <UserInfoContainer />
+          <UserDetailContainer
+            label="שם מלא"
+            value={`${currentUser?.firstName} ${currentUser?.lastName}`}
+          />
+          <UserDetailContainer label="מייל" value={currentUser?.email || ``} />
+          <UserDetailContainer label="טלפון" value={currentUser?.phone || ``} />
+          <UserDetailContainer label="סוג תוכנית" value={currentUser?.planType || ``} />
+          <UserDetailContainer
+            label="תאריך הצטרפות"
+            value={new Date(currentUser?.dateJoined || ``).toLocaleDateString()}
+          />
+          <UserDetailContainer
+            label="תאריך סיום"
+            value={new Date(currentUser?.dateFinished || ``).toLocaleDateString()}
+          />
+          {/* <UserInfoContainer /> */}
           <Button
             mode="contained-tonal"
             style={common.rounded}
