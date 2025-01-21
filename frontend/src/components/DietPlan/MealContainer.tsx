92e2d66fc8bec442cef7268c0f6e0869c85bf31b
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-01-21 16:17:11 +0200

    fixed issue where only extra items would not be shown

--- frontend/src/components/DietPlan/MealContainer.tsx
+++ frontend/src/components/DietPlan/MealContainer.tsx
@@ -42,13 +42,15 @@ const MealContainer: React.FC<MealContainerProps> = ({ meal }) => {
       ]}
     >
       {mealItems.map((mealItem, i) => {
+        const isCustomItems =
+          mealItem[1]?.customItems?.length > 0 || mealItem[1]?.extraItems?.length > 0;
         return (
           <React.Fragment key={mealItem[1]._id + i}>
-            {mealItem[1].customItems && mealItem[1].customItems.length > 0 && (
+            {isCustomItems && (
               <CustomInstructionsContainer item={mealItem[1]} foodGroup={getName(mealItem[0])} />
             )}
 
-            {mealItem[1].quantity > 0 && mealItem[1].customItems.length == 0 && (
+            {mealItem[1].quantity > 0 && !isCustomItems && (
               <MenuItemTicket foodGroup={getName(mealItem[0])} quantity={mealItem[1].quantity} />
             )}
           </React.Fragment>
