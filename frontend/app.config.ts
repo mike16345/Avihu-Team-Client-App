befec38b347c57dc71e400b4b905ca079e836bbc
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-04-30 14:46:14 +0300

    switched config to ts

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,59 +1,66 @@
 import "dotenv/config";
+import { ExpoConfig, ConfigContext } from "@expo/config";
 
-export default ({ config }) => {
+export default ({ config }: ConfigContext): ExpoConfig => {
   const isDev = !!process.env.DEV_MODE;
 
   return {
     ...config,
     name: "Avihu Team",
     slug: "avihu-team",
-    version: "1.0.0",
+    version: "1.1.2",
     orientation: "portrait",
-    icon: "./assets/app-icon.png",
+    icon: "./assets/appstore.png",
     userInterfaceStyle: "dark",
     splash: {
       image: "./assets/avihu/avihuFlyTrapWithLogo.jpeg",
       backgroundColor: "#000000",
-      plugins: [
-        [
-          "expo-image-picker",
-          {
-            photosPermission:
-              "The app accesses your photos to let you share them with your friends.",
-          },
-        ],
-        [
-          "expo-notifications",
-          {
-            icon: "./assets/app-icon.png",
-            color: "#ffffff",
-            defaultChannel: "default",
-            enableBackgroundRemoteNotifications: false,
-          },
-        ],
-      ],
     },
+    plugins: [
+      ["expo-localization"],
+
+      [
+        "expo-image-picker",
+        {
+          photosPermission: "The app accesses your photos to let you share them with your friends.",
+        },
+      ],
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/app-icon.png",
+          color: "#ffffff",
+          defaultChannel: "default",
+          enableBackgroundRemoteNotifications: false,
+        },
+      ],
+    ],
     ios: {
-      bundleIdentifier: isDev
-        ? "com.avihuteam.test" // Development package
-        : "com.avihuteam.avihuteam", // Production
-      supportsTablet: true,
+      bundleIdentifier: isDev ? "com.avihuteam.test" : "com.avihuteam.avihuteam",
+      supportsTablet: false,
       splash: {
         image: "./assets/avihu/avihuFlyTrapWithLogo.jpeg",
         backgroundColor: "#000000",
       },
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/app-icon.png",
+        foregroundImage: "./assets/playstore.png",
         backgroundColor: "#000000",
       },
-      package: "com.avihuteam.avihuteam", // Production package
+
+      package: "com.avihuteam.avihuteam",
     },
     extra: {
       eas: {
         projectId: "bbbbb60d-eb47-48fb-a278-517aba8dcea2",
       },
+      supportsRtl: true,
+      forcesRTL: true,
+
       API_URL: process.env.API_URL,
       API_TOKEN: process.env.API_KEY,
       TRAINER_PHONE_NUMBER: process.env.TRAINER_PHONE_NUMBER,
@@ -64,7 +71,6 @@ export default ({ config }) => {
     runtimeVersion: "1.0.0",
     updates: {
       enabled: true,
-      runtimeVersion: "1.0.0",
       url: "https://u.expo.dev/bbbbb60d-eb47-48fb-a278-517aba8dcea2",
     },
     sdkVersion: "51.0.0",
