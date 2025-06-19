b13817f459d266a326f053bc05ce2ec4113abb0d
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-06-19 15:21:35 +0300

    formatted haptics

--- frontend/src/utils/haptics.ts
+++ frontend/src/utils/haptics.ts
@@ -1,28 +1,28 @@
 import {
-    ImpactFeedbackStyle,
-    NotificationFeedbackType,
-    impactAsync,
-    notificationAsync,
-    selectionAsync
+  ImpactFeedbackStyle,
+  NotificationFeedbackType,
+  impactAsync,
+  notificationAsync,
+  selectionAsync,
 } from "expo-haptics";
 
-const {Heavy,Light,Medium,Rigid,Soft}=ImpactFeedbackStyle;
-const {Error,Success,Warning}=NotificationFeedbackType
+const { Heavy, Light, Medium, Rigid, Soft } = ImpactFeedbackStyle;
+const { Error, Success, Warning } = NotificationFeedbackType;
 
-export const successNotificationHaptic=()=>notificationAsync(Success);
+export const successNotificationHaptic = () => notificationAsync(Success);
 
-export const errorNotificationHaptic=()=>notificationAsync(Error)
+export const errorNotificationHaptic = () => notificationAsync(Error);
 
-export const warningNotificationHaptic=()=>notificationAsync(Warning)
+export const warningNotificationHaptic = () => notificationAsync(Warning);
 
-export const selectionHaptic=()=>selectionAsync()
+export const selectionHaptic = () => selectionAsync();
 
-export const softHaptic=()=>impactAsync(Soft)
+export const softHaptic = () => impactAsync(Soft);
 
-export const rigidHaptic=()=>impactAsync(Rigid)
+export const rigidHaptic = () => impactAsync(Rigid);
 
-export const lightHaptic=()=>impactAsync(Light)
+export const lightHaptic = () => impactAsync(Light);
 
-export const mediumHaptic=()=>impactAsync(Medium)
+export const mediumHaptic = () => impactAsync(Medium);
 
-export const heavyHaptic=()=>impactAsync(Heavy)
+export const heavyHaptic = () => impactAsync(Heavy);
