4c1eaf1b098a5ae4182b92a810e66fbc591c4cf5
Noam Moskowitz <noammz101@gmail.com>
2025-07-15 13:56:12 +0300

    got rid of style on frameShadow

--- frontend/src/components/ui/FrameShadow.tsx
+++ frontend/src/components/ui/FrameShadow.tsx
@@ -1,27 +1,24 @@
-import { View } from 'react-native'
-import { useShadowStyles } from '@/styles/useShadowStyles'
+import { View } from "react-native";
+import { useShadowStyles } from "@/styles/useShadowStyles";
 
 interface FrameShadowProps {
- children: any,
- style?: any[]
+  children: any;
 }
 
-const FrameShadow: React.FC<FrameShadowProps> = ({ children, style }) => {
- const { frameLayer1, frameLayer2, frameLayer3, frameLayer4, frameLayer5 } = useShadowStyles()
+const FrameShadow: React.FC<FrameShadowProps> = ({ children }) => {
+  const { frameLayer1, frameLayer2, frameLayer3, frameLayer4, frameLayer5 } = useShadowStyles();
 
- return (
-   <View style={[frameLayer1, style]}>
-     <View style={frameLayer2}>
-       <View style={frameLayer3}>
-         <View style={frameLayer4}>
-           <View style={frameLayer5}>
-             {children}
-           </View>
-         </View>
-       </View>
-     </View>
-   </View>
- )
-}
+  return (
+    <View style={[frameLayer1]}>
+      <View style={frameLayer2}>
+        <View style={frameLayer3}>
+          <View style={frameLayer4}>
+            <View style={frameLayer5}>{children}</View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default FrameShadow
+export default FrameShadow;
