e9a829aa512e2212bce3f5f21a32016978386401
Noam Moskowitz <noammz101@gmail.com>
2025-07-15 11:56:46 +0300

    added svgs and created icon component, finished primary button component

--- frontend/src/components/ui/buttons/PrimaryButton.tsx
+++ frontend/src/components/ui/buttons/PrimaryButton.tsx
@@ -1,36 +1,70 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import useStyles from '@/styles/useGlobalStyles'
-import NativeIcon from '../../Icon/NativeIcon';
-import { ConditionalRender } from '../ConditionalRender';
-import SpinningIcon from '../loaders/SpinningIcon';
-
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import useStyles from "@/styles/useGlobalStyles";
+import NativeIcon from "../../Icon/NativeIcon";
+import { ConditionalRender } from "../ConditionalRender";
+import SpinningIcon from "../loaders/SpinningIcon";
+import { IconName } from "@/constants/iconMap";
+import Icon from "@/components/Icon/Icon";
 
 interface PrimaryButtonProps {
-label:string;
-mode:'dark'|'light';
-block?:boolean;
-icon?:any;
-loading?:boolean
-  }
-
-const PrimaryButton:React.FC<PrimaryButtonProps> = ({label,mode='dark',block,icon,loading}) => {
-    const {colors,common,fonts,layout,spacing,text}=useStyles()
-
-  return (
-    <TouchableOpacity disabled={loading} style={[layout.center,spacing.pdDefault,common.rounded,mode=='dark'?colors.backgroundPrimary:[colors.backgroundSurface,common.borderXsm,colors.outline],layout.flexRow,spacing.gapDefault,layout.itemsCenter, block?layout.widthFull:{alignSelf:'flex-start'} ]}>
-        <ConditionalRender condition={!loading} children={
-            <> 
-                {icon}
-                <Text style={[mode=='dark'?colors.textOnPrimary:colors.textPrimary,text.textBold,]}>
-                    {label}
-                </Text>
-            </>
-        }/>
-
-        <ConditionalRender condition={loading} children={<SpinningIcon mode={mode}/> }/>
-    </TouchableOpacity>
-  )
+  label: string;
+  mode: "dark" | "light";
+  block?: boolean;
+  icon?: IconName;
+  loading?: boolean;
 }
 
-export default PrimaryButton
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  label,
+  mode = "dark",
+  block,
+  icon,
+  loading,
+}) => {
+  const { colors, common, layout, spacing, text } = useStyles();
+
+  return (
+    <TouchableOpacity
+      disabled={loading}
+      style={[
+        layout.center,
+        spacing.pdDefault,
+        common.rounded,
+        mode == "dark"
+          ? colors.backgroundPrimary
+          : [colors.backgroundSurface, common.borderXsm, colors.outline],
+        layout.flexRow,
+        spacing.gapDefault,
+        layout.itemsCenter,
+        block ? layout.widthFull : { alignSelf: "flex-start" },
+      ]}
+    >
+      <ConditionalRender
+        condition={!loading}
+        children={
+          <>
+            <ConditionalRender
+              condition={icon}
+              children={
+                <Icon
+                  name={icon || "bell"}
+                  color={mode == "dark" ? colors.textOnPrimary.color : colors.textPrimary.color}
+                />
+              }
+            />
+            <Text
+              style={[mode == "dark" ? colors.textOnPrimary : colors.textPrimary, text.textBold]}
+            >
+              {label}
+            </Text>
+          </>
+        }
+      />
+
+      <ConditionalRender condition={loading} children={<SpinningIcon mode={mode} />} />
+    </TouchableOpacity>
+  );
+};
+
+export default PrimaryButton;
--- frontend/src/components/ui/loaders/SpinningIcon.tsx
+++ frontend/src/components/ui/loaders/SpinningIcon.tsx
@@ -1,18 +1,19 @@
-import NativeIcon from '@/components/Icon/NativeIcon';
-import useColors from '@/styles/useColors';
-import React, { useEffect, useRef } from 'react';
-import { Animated, Easing } from 'react-native';
+import Icon from "@/components/Icon/Icon";
+import NativeIcon from "@/components/Icon/NativeIcon";
+import useColors from "@/styles/useColors";
+import React, { useEffect, useRef } from "react";
+import { Animated, Easing } from "react-native";
 
-const SpinningIcon:React.FC<{mode:'dark'|'light'}> = ({mode='dark'}) => {
+const SpinningIcon: React.FC<{ mode: "dark" | "light" }> = ({ mode = "dark" }) => {
   const spinAnim = useRef(new Animated.Value(0)).current;
 
-  const {textPrimary,textOnPrimary}=useColors()
+  const { textPrimary, textOnPrimary } = useColors();
 
   useEffect(() => {
     Animated.loop(
       Animated.timing(spinAnim, {
         toValue: 1,
-        duration: 1000, // 1 second per rotation
+        duration: 1500, // 1 second per rotation
         easing: Easing.linear,
         useNativeDriver: true,
       })
@@ -21,12 +22,16 @@ const SpinningIcon:React.FC<{mode:'dark'|'light'}> = ({mode='dark'}) => {
 
   const spin = spinAnim.interpolate({
     inputRange: [0, 1],
-    outputRange: ['0deg', '360deg'],
+    outputRange: ["0deg", "720deg"],
   });
 
   return (
     <Animated.View style={{ transform: [{ rotate: spin }] }}>
-      <NativeIcon library="AntDesign" name="loading2" size={24} color={mode=='dark'?textOnPrimary.color:textPrimary.color} />
+      <Icon
+        name="loader"
+        color={mode == "dark" ? textOnPrimary.color : textPrimary.color}
+        variant="outline"
+      />
     </Animated.View>
   );
 };
