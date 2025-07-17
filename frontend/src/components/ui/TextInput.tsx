536103936bec2ca13c58ebff227d89a0ab060fb6
Noam Moskowitz <noammz101@gmail.com>
2025-07-17 16:13:40 +0300

    restyled text input

--- frontend/src/components/ui/TextInput.tsx
+++ frontend/src/components/ui/TextInput.tsx
@@ -1,26 +1,53 @@
+import useStyles from "@/styles/useGlobalStyles";
 import { FC, useState } from "react";
+import { View } from "react-native";
 import { TextInput as RNPTextInput, TextInputProps } from "react-native-paper";
+import { ConditionalRender } from "./ConditionalRender";
+import Icon from "../Icon/Icon";
 
-const TextInput: FC<TextInputProps> = ({ style, ...props }) => {
+const TextInput: FC<TextInputProps> = ({ style, error, ...props }) => {
+  const { colors, common } = useStyles();
+
+  const [focused, setFocused] = useState(false);
+
+  const borderColor = error ? colors.borderError : focused ? colors.borderPrimary : colors.outline;
+  const borderwidth = focused ? common.borderSm : common.borderXsm;
+
   return (
-    <RNPTextInput
-      underlineColor="transparent"
-      underlineStyle={{ width: 0 }}
-      contentStyle={{ fontWeight: `bold` }}
-      style={[
-        {
-          borderWidth: 0,
-          textAlign: "right",
-          borderRadius: 9,
-          height: 38,
-          borderTopLeftRadius: 9,
-          borderTopRightRadius: 9,
-          backgroundColor: "rgba(150, 150, 150, 0.5)",
-        },
-        style,
-      ]}
-      {...props}
-    />
+    <View style={{ position: "relative" }}>
+      <RNPTextInput
+        underlineColor="transparent"
+        underlineStyle={{ width: 0 }}
+        cursorColor={colors.textPrimary.color}
+        textColor={colors.textPrimary.color}
+        selectionColor={colors.textPrimary.color}
+        placeholderTextColor="grey"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        contentStyle={{ paddingRight: error ? 30 : 8 }}
+        style={[
+          {
+            borderWidth: 0,
+            textAlign: "right",
+            borderRadius: 9,
+            height: 38,
+            borderTopLeftRadius: 9,
+            borderTopRightRadius: 9,
+          },
+          common.roundedSm,
+          borderColor,
+          borderwidth,
+          colors.background,
+          style,
+        ]}
+        {...props}
+      />
+      <ConditionalRender condition={error}>
+        <View style={[{ position: "absolute", right: 10, top: 12 }]}>
+          <Icon name="info" height={18} width={18} color={colors.textDanger.color} />
+        </View>
+      </ConditionalRender>
+    </View>
   );
 };
 
--- frontend/src/screens/Sandbox.tsx
+++ frontend/src/screens/Sandbox.tsx
@@ -3,9 +3,13 @@ import useStyles from "@/styles/useGlobalStyles";
 import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
 import SecondaryButton from "@/components/ui/buttons/SecondaryButton";
 import IconButton from "@/components/ui/buttons/IconButton";
+import TextInput from "@/components/ui/TextInput";
+import { useState } from "react";
 
 const Sandbox = () => {
   const { colors, spacing, layout } = useStyles();
+
+  const [value, setValue] = useState(undefined);
 
   return (
     <View
@@ -18,38 +22,9 @@ const Sandbox = () => {
         spacing.gapDefault,
       ]}
     >
-      <Text style={[colors.textPrimary]}>Sandbox</Text>
+      <Text style={[colors.textPrimary]}>{value || "type to change me"}</Text>
 
-      <PrimaryButton mode="dark" children="התחברות" block icon="like" loading />
-      <PrimaryButton mode="light" children="התחברות" block icon="like" loading />
-      <PrimaryButton
-        mode="dark"
-        children="התחברות"
-        block
-        icon="like"
-        onPress={() => console.log("pressed")}
-      />
-      <PrimaryButton
-        mode="light"
-        children={<Text style={{ color: "red" }}>kaka batacha</Text>}
-        block
-        icon="like"
-      />
-
-      <View style={[layout.flexRow, layout.wrap, spacing.gapDefault]}>
-        <PrimaryButton mode="dark" children="התחברות" />
-
-        <SecondaryButton children="הסטוריית משקל וחזרות" leftIcon="documentText" />
-        <SecondaryButton children="דגשים" leftIcon="info" />
-        <SecondaryButton children="הצגת תחליף לארוחה בחלבון" leftIcon="info" />
-        <SecondaryButton children={<Text>aaaaaaabbbaa</Text>} rightIcon="arrowLeft" size="sm" />
-        <SecondaryButton children="לצפייה" rightIcon="arrowLeft" size="sm" shadow={false} />
-      </View>
-
-      <View style={[layout.flexRow, layout.wrap, spacing.gapDefault]}>
-        <IconButton icon="gallery" />
-        <IconButton icon="camera" />
-      </View>
+      <TextInput onChangeText={(val) => setValue(val)} placeholder="Michael touch me" />
     </View>
   );
 };
