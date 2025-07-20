29cae4122f0a4fd82022f6c38735d3b344b2a156
Noam Moskowitz <noammz101@gmail.com>
2025-07-20 15:33:33 +0300

    code cleanup

--- frontend/src/screens/Sandbox.tsx
+++ frontend/src/screens/Sandbox.tsx
@@ -1,11 +1,6 @@
 import { View, Text } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
-import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
-import { useToast } from "@/hooks/useToast";
-import ToastContainer from "@/components/ui/toast/ToastContainer";
-import useExerciseMethodApi from "@/hooks/api/useExerciseMethodsApi";
-import { useState } from "react";
-import AsyncToastWrapper from "@/components/ui/AsyncToastWrapper";
+
 import ChatBubble from "@/components/ui/chat/ChatBubble";
 import ChatButton from "@/components/ui/chat/ChatButton";
 
@@ -29,9 +24,9 @@ const Sandbox = () => {
       <ChatBubble
         text="בשמחה, הנה הגרסה המעודכנת והמתונה – ללא אייקונים:
 מספר האימונים המומלץ בשבוע תלוי במטרה האישית שלכם,
- רמת הכושר והזמן הפנוי.  באופן כללי:
-לשיפור כללי או שמירה – 3 עד 4 אימונים בשבוע יכולים להספיק.  להרזיה או חיטוב – 4 עד 5 אימונים בשילוב תזונה מתאימה.  לבניית מסת שריר – 4 עד 6 אימונים בשבוע, עם חלוקה לפי קבוצות שרירים ומנוחה מסודרת.
-הכי חשוב לשמור על עקביות, להקשיב לגוף ולעבוד בצורה שמתאימה לכם.  אימון טוב נמדד לא רק בכמות, אלא גם באיכות."
+ רמת הכושר והזמן הפנוי. באופן כללי:
+לשיפור כללי או שמירה – 3 עד 4 אימונים בשבוע יכולים להספיק. להרזיה או חיטוב – 4 עד 5 אימונים בשילוב תזונה מתאימה. לבניית מסת שריר – 4 עד 6 אימונים בשבוע, עם חלוקה לפי קבוצות שרירים ומנוחה מסודרת.
+הכי חשוב לשמור על עקביות, להקשיב לגוף ולעבוד בצורה שמתאימה לכם. אימון טוב נמדד לא רק בכמות, אלא גם באיכות."
         variant="response"
       />
 
