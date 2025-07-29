eb0339a93d4b640e09d7bf07297638d4d64a9e10
Noam Moskowitz <noammz101@gmail.com>
2025-07-29 10:21:39 +0300

    added wheel pickers to sandbox

--- frontend/src/screens/Sandbox.tsx
+++ frontend/src/screens/Sandbox.tsx
@@ -1,18 +1,14 @@
-import { View, Text, useWindowDimensions } from "react-native";
+import { View, useWindowDimensions } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
-import Collapsible from "@/components/ui/Collapsible";
 import { useState } from "react";
-import Icon from "@/components/Icon/Icon";
-import WheelPicker from "@/components/ui/WheelPicker";
 import { generateWheelPickerData } from "@/utils/utils";
 import WeightWheelPicker from "@/components/WeightGraph/WeightWheelPicker";
+import RepWheelPicker from "@/components/ui/RepWheelPicker";
 
 const Sandbox = () => {
-  const { colors, spacing, layout, common } = useStyles();
-  const { height, width } = useWindowDimensions();
+  const { colors, spacing, layout } = useStyles();
+  const { width } = useWindowDimensions();
 
-  const [open, setOpen] = useState(false);
-  const [open2, setOpen2] = useState(false);
   const [wheelVal, setWheelVal] = useState(12);
   const [weightVal, setWeightVal] = useState(12);
 
@@ -30,55 +26,32 @@ const Sandbox = () => {
       ]}
     >
       <View>
-        <View
-          style={[
-            common.borderXsm,
-            spacing.pdHorizontalDefault,
-            spacing.pdVerticalXs,
-            common.rounded,
-            { width: width * 0.45 },
-          ]}
-        >
-          <WeightWheelPicker
-            onValueChange={(val) => {
-              setWeightVal(val);
-            }}
-            activeItemColor={colors.textPrimary.color}
-            inactiveItemColor={colors.textPrimary.color}
-            minWeight={1}
-            decimalStepSize={25}
-            showZeroDecimal={true}
-            decimalRange={100}
-            maxWeight={500}
-            stepSize={1}
-            label=""
-            height={50}
-            itemHeight={35}
-            selectedWeight={weightVal}
-          />
-        </View>
+        <WeightWheelPicker
+          style={{ width: width * 0.4 }}
+          onValueChange={(val) => {
+            setWeightVal(val);
+          }}
+          activeItemColor={colors.textPrimary.color}
+          inactiveItemColor={colors.textPrimary.color}
+          minWeight={0}
+          decimalStepSize={25}
+          showZeroDecimal={true}
+          decimalRange={100}
+          maxWeight={500}
+          stepSize={1}
+          height={50}
+          itemHeight={35}
+          selectedWeight={weightVal}
+        />
       </View>
 
       <View>
-        <View
-          style={[
-            common.borderXsm,
-            spacing.pdHorizontalDefault,
-            spacing.pdVerticalXs,
-            common.rounded,
-            { width: width * 0.4 },
-          ]}
-        >
-          <WheelPicker
-            activeItemColor={colors.textPrimary.color}
-            inactiveItemColor={colors.textPrimary.color}
-            data={generateWheelPickerData(1, 100)}
-            onValueChange={(val) => setWheelVal(val)}
-            selectedValue={wheelVal}
-            height={50}
-            itemHeight={35}
-          />
-        </View>
+        <RepWheelPicker
+          style={{ width: width * 0.4 }}
+          data={generateWheelPickerData(0, 100)}
+          onValueChange={(val) => setWheelVal(val)}
+          selectedValue={wheelVal}
+        />
       </View>
     </View>
   );
