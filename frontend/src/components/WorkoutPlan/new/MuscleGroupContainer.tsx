20d0b2246baf5137b5380de607bb884493478e5f
Noam Moskowitz <noammz101@gmail.com>
2025-08-13 22:14:28 +0300

    created muscle group container

---
+++ frontend/src/components/WorkoutPlan/new/MuscleGroupContainer.tsx
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+import React from "react";
+import useStyles from "@/styles/useGlobalStyles";
+import { Text } from "@/components/ui/Text";
+import ExerciseContainer from "./ExerciseContainer";
+import { IMuscleGroupWorkouts } from "@/interfaces/Workout";
+
+interface MuscleGroupContainerProps {
+  muscleGroup: IMuscleGroupWorkouts;
+}
+
+const MuscleGroupContainer: React.FC<MuscleGroupContainerProps> = ({
+  muscleGroup: { exercises, muscleGroup },
+}) => {
+  const { colors, common, layout, spacing, text } = useStyles();
+
+  return (
+    <View style={spacing.gapMd}>
+      <View
+        style={[colors.backgroundSurface, common.rounded, spacing.pdDefault, layout.alignSelfStart]}
+      >
+        <Text style={text.textBold}>{muscleGroup}</Text>
+      </View>
+
+      {exercises.map((exercise, i) => (
+        <ExerciseContainer
+          key={exercise._id}
+          exercise={exercise}
+          exerciseCounter={`${i + 1}/${exercises.length}`}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default MuscleGroupContainer;
