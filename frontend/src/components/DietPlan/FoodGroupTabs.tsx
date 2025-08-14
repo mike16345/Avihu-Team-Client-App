724dd440f420c7dbb9f093f1393ce5e94247a024
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-08-14 20:30:28 +0300

    made scroll work

--- frontend/src/components/DietPlan/FoodGroupTabs.tsx
+++ frontend/src/components/DietPlan/FoodGroupTabs.tsx
@@ -1,14 +1,16 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/Tabs";
 import { ReactNode, useMemo, useState } from "react";
 import useMenuItemsQuery from "@/hooks/queries/MenuItems/useMenuItemsQuery";
-import { ScrollView, View } from "react-native";
+import { ScrollView, useWindowDimensions, View } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
 import SpinningIcon from "../ui/loaders/SpinningIcon";
 import { foodGroupToName, formatServingText } from "@/utils/utils";
 import { Text } from "../ui/Text";
 import { Card } from "../ui/Card";
+import { BOTTOM_BAR_HEIGHT } from "@/constants/Constants";
 
 const FoodGroupTabs = () => {
+  const { height } = useWindowDimensions();
   const { layout, spacing } = useStyles();
   const { data, isLoading } = useMenuItemsQuery();
 
@@ -33,7 +35,7 @@ const FoodGroupTabs = () => {
       );
       tabContent.push(
         <TabsContent key={foodGroupTranslated} value={foodGroupTranslated} forceMount>
-          <Card variant="gray">
+          <Card style={{ maxHeight: height / 2 - (BOTTOM_BAR_HEIGHT + 20) }} variant="gray">
             <ScrollView contentContainerStyle={[spacing.gapDefault]}>
               {data[foodGroup].map((item) => {
                 return <Text key={item._id}>{formatServingText(item.name, item.oneServing)}</Text>;
@@ -61,7 +63,7 @@ const FoodGroupTabs = () => {
 
   return (
     <Tabs value={selectedTab} onValueChange={setSelectedTab}>
-      <View style={[spacing.gap20]}>
+      <View style={[spacing.gap20, { paddingBottom: 100 }]}>
         <TabsList>{tabTriggers}</TabsList>
         {tabContent}
       </View>
