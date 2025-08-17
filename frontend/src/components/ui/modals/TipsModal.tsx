2498451f0698f68a9c1ee46138d48ef71b6e33b7
Noam Moskowitz <noammz101@gmail.com>
2025-08-17 15:47:01 +0300

    added ability to pass title to tips modal

--- frontend/src/components/ui/modals/TipsModal.tsx
+++ frontend/src/components/ui/modals/TipsModal.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, View } from "react-native";
-import React from "react";
+import React, { ReactNode } from "react";
 import { CustomModal, CustomModalProps } from "./Modal";
 import useStyles from "@/styles/useGlobalStyles";
 import { ConditionalRender } from "../ConditionalRender";
@@ -8,18 +8,20 @@ import { Text } from "../Text";
 interface TipsModalProps extends CustomModalProps {
   tips?: string[];
   noDataMessage?: string;
+  title?: ReactNode;
 }
 
 const TipsModal: React.FC<TipsModalProps> = ({
   tips = [],
   noDataMessage = "אין דגשים להצגה",
+  title,
   ...props
 }) => {
   const { colors, common, layout, spacing } = useStyles();
 
   return (
     <CustomModal {...props}>
-      <CustomModal.Header>דגשים לאימון</CustomModal.Header>
+      <CustomModal.Header>{title}</CustomModal.Header>
       <CustomModal.Content>
         <ConditionalRender condition={tips.length == 0}>
           <View style={[layout.flex1, layout.center]}>
