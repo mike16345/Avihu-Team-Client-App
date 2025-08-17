91831c079221676832c53dfabf1e7cf1174daaae
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-08-17 14:48:10 +0300

    fetched diet plan

--- frontend/src/components/DietPlan/DietPlanScreenHeader.tsx
+++ frontend/src/components/DietPlan/DietPlanScreenHeader.tsx
@@ -3,21 +3,30 @@ import useStyles from "@/styles/useGlobalStyles";
 import SecondaryButton from "../ui/buttons/SecondaryButton";
 import DailyCalorieIntake from "./DailyCalorieIntake";
 import { Text } from "../ui/Text";
+import TipsModal from "../ui/modals/TipsModal";
+import { useState } from "react";
+import useDietPlanQuery from "@/hooks/queries/useDietPlanQuery";
 
 const DietPlanScreenHeader = () => {
   const { layout, spacing } = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+  const { data } = useDietPlanQuery();
+  const tips = data?.customInstructions || [];
 
   return (
-    <View style={[layout.flexRow, spacing.gapLg, layout.itemsCenter]}>
-      <View style={[layout.flexRow, layout.widthFull, { flex: 2 }]}>
-        <DailyCalorieIntake />
+    <>
+      <View style={[layout.flexRow, spacing.gapLg, layout.itemsCenter]}>
+        <View style={[layout.flexRow, layout.widthFull, { flex: 2 }]}>
+          <DailyCalorieIntake />
+        </View>
+        <View style={[layout.flexRow, layout.center, { flex: 1 }]}>
+          <SecondaryButton onPress={() => setIsOpen(true)} rightIcon="info">
+            <Text fontVariant="semibold">דגשים</Text>
+          </SecondaryButton>
+        </View>
       </View>
-      <View style={[layout.flexRow, layout.center, { flex: 1 }]}>
-        <SecondaryButton rightIcon="info">
-          <Text fontVariant="semibold">דגשים</Text>
-        </SecondaryButton>
-      </View>
-    </View>
+      <TipsModal visible={isOpen} onDismiss={() => setIsOpen(false)} tips={tips} />
+    </>
   );
 };
 
