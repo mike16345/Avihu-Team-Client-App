d4eee007ff194fb442deb27cbb70955c570c0166
Noam Moskowitz <noammz101@gmail.com>
2025-08-19 09:32:46 +0300

    removed state for label for use of useMemo

--- frontend/src/components/ui/dropwdown/DropDownTrigger.tsx
+++ frontend/src/components/ui/dropwdown/DropDownTrigger.tsx
@@ -1,23 +1,23 @@
 import { View, Text } from "react-native";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import PrimaryButton from "../buttons/PrimaryButton";
 import useStyles from "@/styles/useGlobalStyles";
 import { useDropwDownContext } from "@/context/useDropdown";
 import Icon from "@/components/Icon/Icon";
+
+const DEFAULT_LABEL = "בחר";
 
 const DropDownTrigger = () => {
   const { colors, common, layout, spacing } = useStyles();
 
   const { setShouldCollapse, shouldCollapse, selectedValue, items } = useDropwDownContext();
 
-  const [label, setLabel] = useState(items.length == 0 ? "בחר" : items[0].label);
-
-  useEffect(() => {
-    if (!selectedValue) return;
+  const label = useMemo(() => {
+    if (items.length === 0 || !selectedValue) return DEFAULT_LABEL;
 
     const selected = items.find((items) => items.value == selectedValue);
 
-    setLabel(selected?.label || "בחר");
+    return selected?.label || DEFAULT_LABEL;
   }, [selectedValue]);
 
   return (
