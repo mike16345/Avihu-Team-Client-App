c957238f2ec4aead1e86a27510566f9f1f4cdca5
Noam Moskowitz <noammz101@gmail.com>
2025-08-19 16:53:05 +0300

    WIP working with server response

--- frontend/src/components/WorkoutProgression/WorkoutProgressionHeader.tsx
+++ frontend/src/components/WorkoutProgression/WorkoutProgressionHeader.tsx
@@ -1,21 +1,21 @@
 import { View } from "react-native";
-import { useState } from "react";
 import MuscleGroupSelector from "./MuscleGroupSelector";
-
-import useStyles from "@/styles/useGlobalStyles";
-import { MUSCLE_GROUPS, MUSCLE_GROUPS_ENGLISH, muscleImageName } from "@/constants/muscleGroups";
-
 import ExerciseSelector from "./ExerciseSelector";
 
-const WorkoutProgressionHeader = () => {
-  const { colors, common, fonts, layout, spacing, text } = useStyles();
-  const [activeMuscleGroup, setActiveMuscleGroup] = useState(MUSCLE_GROUPS[0]);
+interface WorkoutProgressionHeaderProps {
+  handleMuscleGroupSelect: (muscle: string) => void;
+  activeMuscleGroup: string;
+}
 
+const WorkoutProgressionHeader: React.FC<WorkoutProgressionHeaderProps> = ({
+  handleMuscleGroupSelect,
+  activeMuscleGroup,
+}) => {
   return (
     <View>
       <MuscleGroupSelector
         selectedMuscleGroup={activeMuscleGroup}
-        onMuscleGroupSelect={(muscle) => setActiveMuscleGroup(muscle)}
+        onMuscleGroupSelect={handleMuscleGroupSelect}
       />
 
       <ExerciseSelector muscleGroup={activeMuscleGroup} />
--- frontend/src/screens/Windows/WorkoutProgressionWindow.tsx
+++ frontend/src/screens/Windows/WorkoutProgressionWindow.tsx
@@ -1,18 +1,41 @@
 import Icon from "@/components/Icon/Icon";
 import Collapsible from "@/components/ui/Collapsible";
+import { ConditionalRender } from "@/components/ui/ConditionalRender";
 import Graph from "@/components/ui/graph/Graph";
 import { Text } from "@/components/ui/Text";
 import WorkoutProgressionHeader from "@/components/WorkoutProgression/WorkoutProgressionHeader";
+import { MUSCLE_GROUPS } from "@/constants/muscleGroups";
 import { DropDownContextProvider } from "@/context/useDropdown";
+import useRecordedSetsQuery from "@/hooks/queries/RecordedSets/useRecordedSetsQuery";
 import useStyles from "@/styles/useGlobalStyles";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { View, ScrollView, TouchableOpacity } from "react-native";
 
 const WorkoutProgressionWindow = () => {
   const { layout, colors, common, fonts, spacing, text } = useStyles();
 
+  const [activeMuscleGroup, setActiveMuscleGroup] = useState<string>(MUSCLE_GROUPS[0]);
   const [showRepsGraph, setShowRepsGraph] = useState(true);
-  const [showWeightGraph, setShowWeightGraph] = useState(false);
+  const [showWeightGraph, setShowWeightGraph] = useState(true);
+
+  const { data, isLoading, error, isError } = useRecordedSetsQuery();
+
+  const exercisesByMuscleGroups = useMemo(() => {
+    if (!data) return [];
+
+    return data.reduce((acc, current) => {
+      const { muscleGroup, recordedSets } = current;
+      const currentMuscleGroupExerciseNames = Object.keys(recordedSets);
+
+      acc[muscleGroup] = currentMuscleGroupExerciseNames;
+
+      return acc;
+    }, {});
+  }, [data]);
+
+  const exercises = useMemo(() => {
+    return exercisesByMuscleGroups[activeMuscleGroup] ?? [];
+  }, [exercisesByMuscleGroups, activeMuscleGroup]);
 
   const handleCollapseChange = (open: boolean) => {
     setShowRepsGraph(!open);
@@ -21,63 +44,85 @@ const WorkoutProgressionWindow = () => {
 
   return (
     <View style={[spacing.gapDefault, layout.flex1, spacing.pdMd]}>
-      <DropDownContextProvider
-        items={Array.from({ length: 20 }).map((_, i) => {
-          return { label: i, value: i };
-        })}
-        onSelect={() => {}}
-      >
+      <DropDownContextProvider items={exercises} onSelect={() => {}}>
         <View style={{ zIndex: 10 }}>
-          <WorkoutProgressionHeader />
+          <WorkoutProgressionHeader
+            activeMuscleGroup={activeMuscleGroup}
+            handleMuscleGroupSelect={(val) => setActiveMuscleGroup(val)}
+          />
         </View>
       </DropDownContextProvider>
 
-      <ScrollView contentContainerStyle={spacing.gapDefault}>
-        <Collapsible
-          isCollapsed={showRepsGraph}
-          onCollapseChange={() => handleCollapseChange(showRepsGraph)}
-          trigger={
-            <View style={[layout.flexRow, layout.itemsCenter, layout.justifyBetween]}>
-              <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
-                <Icon name="clock" />
-                <Text>חזרות</Text>
-              </View>
+      <ConditionalRender condition={exercises.length === 0}>
+        <View style={[layout.center]}>
+          <Text>לא הוקלטו סטים</Text>
+        </View>
+      </ConditionalRender>
 
-              <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
-                <Text>60.00%</Text>
-                <Icon name="growthIndicator" />
-              </View>
-            </View>
-          }
-        >
-          <Graph
-            data={[12, 320, 3, 67, 3, 456, 4]}
-            labels={["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]}
-          />
-        </Collapsible>
-        <Collapsible
-          isCollapsed={showWeightGraph}
-          onCollapseChange={(val) => handleCollapseChange(val)}
-          trigger={
-            <View style={[layout.flexRow, layout.itemsCenter, layout.justifyBetween]}>
-              <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
-                <Icon name="upload" />
-                <Text>משקל</Text>
-              </View>
+      <ConditionalRender condition={exercises.length !== 0}>
+        <ScrollView contentContainerStyle={spacing.gapDefault} nestedScrollEnabled>
+          <Collapsible
+            isCollapsed={showRepsGraph}
+            onCollapseChange={() => handleCollapseChange(showRepsGraph)}
+            trigger={
+              <View style={[layout.flexRow, layout.itemsCenter, layout.justifyBetween]}>
+                <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
+                  <Icon name="clock" />
+                  <Text>חזרות</Text>
+                </View>
 
-              <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
-                <Text>60.00%</Text>
-                <Icon name="growthIndicator" />
+                <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
+                  <Text>60.00%</Text>
+                  <Icon name="growthIndicator" />
+                </View>
               </View>
-            </View>
-          }
-        >
-          <Graph
-            data={[12, 320, 3, 67, 3, 456, 4]}
-            labels={["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]}
-          />
-        </Collapsible>
-      </ScrollView>
+            }
+          >
+            <Graph
+              data={[12, 320, 3, 67, 3, 456, 4]}
+              labels={[
+                "sunday",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday",
+              ]}
+            />
+          </Collapsible>
+          <Collapsible
+            isCollapsed={showWeightGraph}
+            onCollapseChange={(val) => handleCollapseChange(val)}
+            trigger={
+              <View style={[layout.flexRow, layout.itemsCenter, layout.justifyBetween]}>
+                <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
+                  <Icon name="upload" />
+                  <Text>משקל</Text>
+                </View>
+
+                <View style={[layout.flexRow, spacing.gapDefault, layout.itemsCenter]}>
+                  <Text>60.00%</Text>
+                  <Icon name="growthIndicator" />
+                </View>
+              </View>
+            }
+          >
+            <Graph
+              data={[12, 320, 3, 67, 3, 456, 4]}
+              labels={[
+                "sunday",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday",
+              ]}
+            />
+          </Collapsible>
+        </ScrollView>
+      </ConditionalRender>
     </View>
   );
 };
