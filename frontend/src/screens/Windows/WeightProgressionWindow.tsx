a8831ef2f6c3153a634b6d35e408850d48997dc2
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-08-20 17:19:29 +0300

    chore: removed unused imports

--- frontend/src/screens/Windows/WeightProgressionWindow.tsx
+++ frontend/src/screens/Windows/WeightProgressionWindow.tsx
@@ -1,9 +1,4 @@
-import { View } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
-import { useMemo, useState } from "react";
-import WeightCard from "@/components/WeightGraph/WeightCard";
-import useWeighInsQuery from "@/hooks/queries/WeighIns/useWeighInsQuery";
-import DateUtils from "@/utils/dateUtils";
 import { ScrollView } from "react-native-gesture-handler";
 import Progression from "@/components/WeightProgression/Progression";
 import WeightInput from "@/components/WeightGraph/WeightInput";
