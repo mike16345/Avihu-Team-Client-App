4256d4c20a43e5e02d8d05ab4a6204a6be32f63b
Noam Moskowitz <noammz101@gmail.com>
2025-08-21 20:01:25 +0300

    restyled top bar

--- frontend/src/navigators/TopBar.tsx
+++ frontend/src/navigators/TopBar.tsx
@@ -1,23 +1,18 @@
-import { TouchableOpacity, View, useWindowDimensions } from "react-native";
-
+import { StyleSheet, TouchableOpacity, View, useWindowDimensions } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
-import NativeIcon from "@/components/Icon/NativeIcon";
-import { useUserDrawer } from "@/store/userDrawerStore";
 import { useUserStore } from "@/store/userStore";
 import { Text } from "@/components/ui/Text";
 import Icon from "@/components/Icon/Icon";
-import { ConditionalRender } from "@/components/ui/ConditionalRender";
+import Avatar from "@/components/User/Avatar";
 
 export default function TopBar() {
   const { colors, layout, spacing, common, fonts, text } = useStyles();
   const { height } = useWindowDimensions();
-  const { setOpenUserDrawer } = useUserDrawer();
   const { currentUser } = useUserStore();
 
   return (
     <View
       style={[
-        { paddingTop: spacing.pdStatusBar.paddingTop! + 5 },
         layout.itemsCenter,
         layout.justifyBetween,
         layout.flexRow,
@@ -26,25 +21,12 @@ export default function TopBar() {
           height: height * 0.12,
           borderBottomWidth: 0.25,
           borderBottomColor: colors.borderBackground.borderColor,
+          paddingTop: spacing.pdStatusBar.paddingTop! + 5,
         },
       ]}
     >
       <View style={[layout.flexRow, layout.itemsCenter, spacing.gapDefault]}>
-        <ConditionalRender condition={!currentUser?.profileImage}>
-          <View
-            style={[
-              layout.center,
-              colors.backgroundSecondary,
-              common.roundedFull,
-              spacing.pdHorizontalMd,
-              spacing.pdVerticalXs,
-            ]}
-          >
-            <Text fontVariant="bold" style={fonts.lg}>
-              {currentUser?.firstName.charAt(0)}
-            </Text>
-          </View>
-        </ConditionalRender>
+        <Avatar />
 
         <Text style={fonts.lg}>ברוך הבא {currentUser?.firstName}</Text>
       </View>
@@ -52,21 +34,19 @@ export default function TopBar() {
       <TouchableOpacity style={{ position: "relative" }}>
         <Icon name="bell" height={30} width={30} />
 
-        <View
-          style={[
-            layout.center,
-            common.roundedFull,
-            {
-              backgroundColor: "#95FDA8",
-              position: "absolute",
-              paddingHorizontal: 5,
-              top: -5,
-            },
-          ]}
-        >
+        <View style={[layout.center, common.roundedFull, styles.notificationBadge]}>
           <Text fontVariant="bold">2</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  notificationBadge: {
+    backgroundColor: "#95FDA8",
+    position: "absolute",
+    paddingHorizontal: 5,
+    top: -5,
+  },
+});
