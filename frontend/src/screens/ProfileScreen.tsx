5c8e98c38e99b9b7e5463e0f5a49f69710b126fc
Noam Moskowitz <noammz101@gmail.com>
2025-08-27 15:33:17 +0300

    cleanup up code

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -1,26 +1,17 @@
-import {
-  Image,
-  LayoutChangeEvent,
-  ScrollView,
-  TouchableOpacity,
-  useWindowDimensions,
-  View,
-} from "react-native";
-import React, { useMemo, useState } from "react";
+import React from "react";
 import useStyles from "@/styles/useGlobalStyles";
-import { useUserStore } from "@/store/userStore";
-import UserDetailContainer from "@/components/User/UserDetailContainer";
 import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
 import useLogout from "@/hooks/useLogout";
 import { Text } from "@/components/ui/Text";
 import { RootStackParamList, StackNavigatorProps } from "@/types/navigatorTypes";
 import ProfileHeading from "@/components/User/ProfileHeading";
+import UserDetailsWrapper from "@/components/User/UserDetailsWrapper";
+import { ScrollView } from "react-native";
 
 export type ProfileScreenProps = StackNavigatorProps<RootStackParamList, "Profile">;
 
 const ProfileScreen: React.FC<ProfileScreenProps> = ({ route, navigation }) => {
-  const { colors, common, fonts, layout, spacing, text } = useStyles();
-  const { currentUser } = useUserStore();
+  const { layout, spacing } = useStyles();
   const { handleLogout } = useLogout();
 
   const onLogOut = async () => {
@@ -31,25 +22,12 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({ route, navigation }) => {
   return (
     <ScrollView
       style={[spacing.pdBottomBar, spacing.pdStatusBar]}
-      contentContainerStyle={[layout.flex1, spacing.gap20, spacing.pdLg]}
+      contentContainerStyle={[layout.flex1, spacing.gapXl, spacing.pdLg]}
     >
       <ProfileHeading navigation={navigation} route={route} />
 
-      <UserDetailContainer
-        label="שם מלא"
-        value={`${currentUser?.firstName} ${currentUser?.lastName}`}
-      />
-      <UserDetailContainer label="מייל" value={currentUser?.email || ``} />
-      <UserDetailContainer label="טלפון" value={currentUser?.phone || ``} />
-      <UserDetailContainer label="סוג תוכנית" value={currentUser?.planType || ``} />
-      <UserDetailContainer
-        label="תאריך הצטרפות"
-        value={new Date(currentUser?.dateJoined || ``).toLocaleDateString()}
-      />
-      <UserDetailContainer
-        label="תאריך סיום"
-        value={new Date(currentUser?.dateFinished || ``).toLocaleDateString()}
-      />
+      <UserDetailsWrapper />
+
       <PrimaryButton block mode="light" onPress={onLogOut}>
         <Text fontVariant="bold">התנתק</Text>
       </PrimaryButton>
