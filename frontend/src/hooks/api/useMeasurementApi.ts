de4eb9ee7de900d516db2cfb8eaa6afe0d7f95f0
Noam Moskowitz <noammz101@gmail.com>
2025-09-17 14:11:16 +0300

    created measurements api hook

---
+++ frontend/src/hooks/api/useMeasurementApi.ts
@@ -0,0 +1,27 @@
+import { deleteItem, fetchData, sendData } from "@/API/api";
+import { IUserMuscleMeasurements } from "@/interfaces/measurements";
+import { ApiResponse } from "@/types/ApiTypes";
+
+const MEASUREMENT_ENDPOINT = "muscleMeasurements";
+
+export const useMeasurementApi = () => {
+  const saveMeasurement = (userId: string, date: string, measurement: string, muscle: string) =>
+    sendData<ApiResponse<IUserMuscleMeasurements>>(MEASUREMENT_ENDPOINT, {
+      userId,
+      date,
+      measurement,
+      muscle,
+    });
+
+  const getMeasurements = (userId: string) =>
+    fetchData<ApiResponse<IUserMuscleMeasurements>>(MEASUREMENT_ENDPOINT, userId);
+
+  const removeMeasurement = (userId: string, muscle: string, date: string) =>
+    deleteItem<ApiResponse<IUserMuscleMeasurements>>(MEASUREMENT_ENDPOINT, {
+      userId,
+      muscle,
+      date,
+    });
+
+  return { removeMeasurement, saveMeasurement, getMeasurements };
+};
