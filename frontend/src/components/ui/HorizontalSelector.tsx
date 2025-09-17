2a522715783ac90089e845ed0d314c9cb586807a
Noam Moskowitz <noammz101@gmail.com>
2025-09-17 13:56:24 +0300

    replaced muscle group selector with horizontal selector

---
+++ frontend/src/components/ui/HorizontalSelector.tsx
@@ -0,0 +1,47 @@
+import { ScrollView, View } from "react-native";
+import { Text } from "./Text";
+import PrimaryButton from "./buttons/PrimaryButton";
+import useStyles from "@/styles/useGlobalStyles";
+
+interface HorizontalSelectorProps {
+  selected: string;
+  onSelect: (item: string) => void;
+  items: string[];
+}
+
+const HorizontalSelector: React.FC<HorizontalSelectorProps> = ({
+  onSelect,
+  selected,
+  items = [],
+}) => {
+  const { colors, spacing, layout } = useStyles();
+
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        contentContainerStyle={[spacing.gapDefault, layout.alignSelfStart]}
+        nestedScrollEnabled
+      >
+        {items.map((item, i) => {
+          const isActive = item === selected;
+
+          return (
+            <PrimaryButton
+              key={i}
+              mode={isActive ? "dark" : "light"}
+              onPress={() => onSelect(item)}
+              style={spacing.pdVerticalSm}
+            >
+              <View style={[spacing.pdHorizontalDefault]}>
+                <Text style={isActive ? colors.textOnPrimary : colors.textPrimary}>{item}</Text>
+              </View>
+            </PrimaryButton>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HorizontalSelector;
