a0349c76f500df5c034c2fd60ad428bfabfa83c0
Noam Moskowitz <noammz101@gmail.com>
2025-09-17 13:57:42 +0300

    created measurementInput components ui

---
+++ frontend/src/components/measurements/MeasurementInput.tsx
@@ -0,0 +1,56 @@
+import { View, Image, TouchableOpacity, useWindowDimensions } from "react-native";
+import { Card } from "../ui/Card";
+import useStyles from "@/styles/useGlobalStyles";
+import meausrementPhoto from "@assets/measurement_photo.png";
+import { Text } from "../ui/Text";
+import { MEASUREMENT_DESCRIPTIONS, MeasurementMuscle } from "@/constants/measurements";
+import Input from "../ui/inputs/Input";
+import PrimaryButton from "../ui/buttons/PrimaryButton";
+
+interface MeasurementInputProps {
+  activeMuscleGroup: string;
+}
+
+const MeasurementInput: React.FC<MeasurementInputProps> = ({ activeMuscleGroup }) => {
+  const { width } = useWindowDimensions();
+  const { colors, common, layout, spacing, text } = useStyles();
+
+  return (
+    <Card variant="gray" style={[{ padding: 0 }, layout.flex1, layout.widthFull, common.roundedMd]}>
+      <Card.Header>
+        <Image
+          source={meausrementPhoto}
+          height={0.5}
+          style={[layout.widthFull, common.roundedMd]}
+        />
+      </Card.Header>
+      <Card.Content style={[spacing.pdLg, spacing.gap20]}>
+        <Text style={[text.textCenter]} fontSize={16}>
+          {MEASUREMENT_DESCRIPTIONS[activeMuscleGroup as MeasurementMuscle]}
+        </Text>
+
+        <View style={[layout.flexRow, spacing.gap20, layout.justifyCenter, layout.itemsCenter]}>
+          <TouchableOpacity>
+            <Text fontSize={16} fontVariant="bold">
+              לצפייה בהיקפים
+            </Text>
+          </TouchableOpacity>
+
+          <Input
+            placeholder="רשמו כאן את המידה"
+            keyboardType="decimal-pad"
+            style={[colors.outline]}
+          />
+        </View>
+
+        <View style={[layout.center]}>
+          <PrimaryButton block style={{ width: width * 0.6 }}>
+            שליחה
+          </PrimaryButton>
+        </View>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default MeasurementInput;
