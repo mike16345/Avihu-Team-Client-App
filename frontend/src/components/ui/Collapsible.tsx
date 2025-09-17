e2f3848db8093be317c657db9720032ff3c480df
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-09-17 19:39:01 +0300

    fixed rotation issue

--- frontend/src/components/ui/Collapsible.tsx
+++ frontend/src/components/ui/Collapsible.tsx
@@ -115,7 +115,7 @@ const Collapsible: React.FC<CollapsibleProps> = ({
             <ConditionalRender condition={typeof trigger === "string"}>
               <View style={[layout.flexRow, layout.itemsCenter, layout.justifyBetween]}>
                 <Text {...triggerProps}>{trigger}</Text>
-                <Icon name="chevronDown" rotation={45} />
+                <Icon name="chevronDown" rotation={isCollapsed ? 0 : 180} />
               </View>
             </ConditionalRender>
 
