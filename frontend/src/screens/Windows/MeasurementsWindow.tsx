8e20ea81ca228166dc720b783cd86d5e462d876c
Noam Moskowitz <noammz101@gmail.com>
2025-09-17 13:57:18 +0300

    created ui for measurements page

--- frontend/src/screens/Windows/MeasurementsWindow.tsx
+++ frontend/src/screens/Windows/MeasurementsWindow.tsx
@@ -1,13 +1,47 @@
-import { View, Text } from "react-native";
+import { KeyboardAvoidingView, ScrollView } from "react-native";
 import useStyles from "@/styles/useGlobalStyles";
+import { Text } from "@/components/ui/Text";
+import HorizontalSelector from "@/components/ui/HorizontalSelector";
+import { MEASUREMENT_MUSCLE_GROUPS } from "@/constants/measurements";
+import { useState } from "react";
+import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
+import MeasurementInput from "@/components/measurements/MeasurementInput";
 
 const MeasurementsWindow = () => {
-  const { layout } = useStyles();
+  const { layout, colors, common, fonts, spacing, text } = useStyles();
+
+  const [activeMuscleGroup, setActiveMuscleGroup] = useState<string>(MEASUREMENT_MUSCLE_GROUPS[0]);
 
   return (
-    <View style={[layout.center]}>
-      <Text>MeasurementsWindow</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={[layout.flex1]}>
+      <ScrollView
+        nestedScrollEnabled
+        style={layout.flex1}
+        contentContainerStyle={[spacing.gap20, spacing.pdLg]}
+      >
+        <Text style={layout.alignSelfStart} fontSize={16}>
+          מדידת היקפים
+        </Text>
+
+        <HorizontalSelector
+          items={MEASUREMENT_MUSCLE_GROUPS}
+          selected={activeMuscleGroup}
+          onSelect={(selected) => setActiveMuscleGroup(selected)}
+        />
+
+        <MeasurementInput activeMuscleGroup={activeMuscleGroup} />
+
+        <PrimaryButton mode="light" block icon="camera">
+          <Text fontSize={16} fontVariant="bold">
+            העלאת תמונת התקדמות
+          </Text>
+        </PrimaryButton>
+
+        <Text style={text.textCenter} fontVariant="light">
+          העלו תמונה של ההתקדמות שלכם למאמן
+        </Text>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
