ddb473de440afbd6bcbde1a8b8cf48c11e4308f3
Noam Moskowitz <noammz101@gmail.com>
2025-10-15 16:39:17 +0300

    created notification store

---
+++ frontend/src/store/notificationStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { lightHaptic } from "@/utils/haptics";
+
+export interface INotification {
+  id: string;
+  title: string | null;
+  body: string | null;
+  data: any;
+  createdAt: string | Date;
+}
+
+interface INotificationStore {
+  notifications: INotification[];
+  addNotification: (notification: INotification) => void;
+  removeNotification: (id: string) => void;
+}
+
+export const useNotificationStore = create<INotificationStore>((set, get) => ({
+  notifications: [],
+
+  addNotification: (notification: INotification) => {
+    set((state) => ({
+      notifications: [notification, ...state.notifications],
+    }));
+  },
+
+  removeNotification: (id: string) => {
+    set((state) => ({
+      notifications: state.notifications.filter((n) => n.id !== id),
+    }));
+
+    lightHaptic();
+  },
+}));
