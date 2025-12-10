51ee0aacc6038a7e15715e93dfb87d7d1a779cfe
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-12-10 16:59:22 +0200

    Update to next version

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: "Avihu Team",
     slug: "avihu-team",
-    version: "2.0.3",
+    version: "2.0.5",
     orientation: "portrait",
     icon: "./assets/app-logo.png",
     userInterfaceStyle: "automatic",
