b16597944855f05bdab47210c4b65f85f39cba03
mike16345 <95699872+mike16345@users.noreply.github.com>
2026-03-19 16:50:16 +0200

    chore: Bump version number

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: "Avihu Team",
     slug: "avihu-team",
-    version: "2.2.0",
+    version: "2.2.1",
     orientation: "portrait",
     icon: "./assets/app-logo.png",
     userInterfaceStyle: "automatic",
